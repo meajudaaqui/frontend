ba81204f1c15723f24ecb06679f82f80b0768fb7
danilo siqueira <danilosic.oliveira@gmail.com>
2022-06-15 18:33:36 -0300

    criando pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
+import React from 'react'
 import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
 
 function App() {
+  const [clicks, setClicks] = React.useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Alteração
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Alteração
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //     <button onClick={()=> setClicks(clicks+1)} className={()=>{ if(clicks % 2 == 0) return "par"; else return "impar"; }}>
+    //       {clicks}
+    //     </button>
+    //   </header>
+    // </div>
   );
 }
 
